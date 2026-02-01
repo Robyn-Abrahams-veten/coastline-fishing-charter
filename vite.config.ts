a4bc8087431ee3aa53c998790be3db0e7806bbdb
Robyn Abrahams <vetenstudios@gmail.com>
2026-02-01 22:09:43 +0200

    set base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/coastline-fishing-charter/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
